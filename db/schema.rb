9d706622b0d1190c858d92b228ebb993972aa554
flaviogf <flavio.fernandes6@gmail.com>
2021-06-03 21:23:12 +0000

    feat: adding user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_232504) do
+ActiveRecord::Schema.define(version: 2021_06_03_210349) do
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_05_26_232504) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ test/models/user_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test 'save should return false when name is empty' do
+    user = User.new name: '', email: 'frank@email.com', password: 'test123', password_confirmation: 'test123'
+
+    assert_not user.save
+  end
+
+  test 'save should return false when email is empty' do
+    user = User.new name: 'frank', email: '', password: 'test123', password_confirmation: 'test123'
+
+    assert_not user.save
+  end
+
+  test 'save should return false when email is already taken' do
+    frank = User.new name: 'frank', email: 'frank@email.com', password: 'test123', password_confirmation: 'test123'
+
+    frank.save
+
+    fake = User.new name: 'fake', email: 'frank@email.com', password: 'test123', password_confirmation: 'test123'
+
+    assert_not fake.save
+  end
+
+  test 'save should return false when password is empty' do
+    user = User.new name: 'frank', email: 'frank@email.com', password: '', password_confirmation: 'test123'
+
+    assert_not user.save
+  end
+
+  test 'save should return false when password and password_confirmation do not match' do
+    user = User.new name: 'frank', email: 'frank@email.com', password: 'test123', password_confirmation: 'test1234'
+
+    assert_not user.save
+  end
+end
