9072e248e7b0463228ff44d88afd44cbc57f4e0b
flaviogf <flavio.fernandes6@gmail.com>
2021-06-11 19:24:58 -0300

    feat: adding default roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,14 @@
 class Role < ApplicationRecord
+  ADMIN = 1
+  STAFF = 2
+
   has_many :users
+
+  def self.admin
+    Role.find(ADMIN)
+  end
+
+  def self.staff
+    Role.find(STAFF)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+role = Role.new(name: 'Admin')
+role.id = 1
+role.save!
+
+role = Role.create(name: 'Staff')
+role.id = 2
+role.save!
