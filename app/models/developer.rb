5d86ba28e4d8543f128f546b5e5eae1d01b66740
flaviogf <flavio.fernandes6@gmail.com>
2021-06-14 12:50:44 -0300

    feat: updating developer associations
    updating skill associations

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,3 +1,8 @@
 class Developer < ApplicationRecord
+  has_many :developer_skills, dependent: :destroy
+  has_many :skills, through: :developer_skills
+
   validates :email, presence: true, email: true, uniqueness: true
+
+  accepts_nested_attributes_for :developer_skills
 end
